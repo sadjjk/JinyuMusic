701dab4073ebb96c3802afd67c9316c430e6fa29
sadjjk <sadjjk@163.com>
2021-02-27 20:21:15 +0800

    【Fix】删除虾米音乐

--- Music/platforms/__init__.py
+++ Music/platforms/__init__.py
@@ -2,7 +2,7 @@ from .base import BaseMusic
 from .netease import NeteaseMusic
 from .qq import QQMusic
 from .migu import MiguMusic
-from .xiami import XiamiMusic
+# from .xiami import XiamiMusic
 from .kugou import KugouMusic
 from .kuwo import KuwoMusic
 from .qianqian import QianqianMusic
